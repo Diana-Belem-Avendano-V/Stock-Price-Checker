9d3a2690507a6ff0d9fa997d688565bfe3fc7e5b
Diana-Belem-Avendano-V <paradianaa2@gmail.com>
2025-03-14 16:13:39 -0600

    security policies added

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,22 @@
 'use strict';
 
+const mongodb = require('mongodb')
+const mongoose = require('mongoose')
+
 module.exports = function (app) {
 
+  let uri ='mongodb+srv://Diana:uBguB6uEqPGKa3Xf@cluster0.i95ex.mongodb.net/stock_price_checker?retryWrites=true&w=majority&appName=Cluster0'
+
+  mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true })
+
+  let stockSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    likes: {type: Number, default: 0},
+    ips: [String]
+  })
+  
+  let Stock = mongoose.model('Stock', stockSchema)
+  
   app.route('/api/stock-prices')
     .get(function (req, res){
       
